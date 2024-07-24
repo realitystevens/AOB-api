7adbe02d1073cda3b0a78f33f31693bb7b7c7d1d
Reality Stevens <stevensreality@outlook.com>
2024-07-24 01:29:21 +0000

    [2]: Configured for Github Workflow

--- aob/asgi.py
+++ aob/asgi.py
@@ -8,18 +8,8 @@ if config('ENVIRONMENT') == 'DEVELOPMENT':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.development')
 elif config('ENVIRONMENT') == 'PRODUCTION':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.production')
-elif config('ENVIRONMENT') == 'WORKSPACE':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.workspace')
-elif os.environ.get('ENVIRONMENT') == 'WORKSPACE':
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.workspace')
 else:
-    try:
-        from django.core.exceptions import ImproperlyConfigured
-    except ImproperlyConfigured:
-        raise ImproperlyConfigured(
-        "Couldn't detect current environment."
-        "Please set the ENVIRONMENT variable to either 'DEVELOPMENT', 'PRODUCTION', or 'WORKSPACE'"
-    )
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.workspace')
 
 
 application = get_asgi_application()
