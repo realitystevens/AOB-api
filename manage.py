dad5ebb76fa8d8531d8b958cf01c3606df7adf52
Reality Stevens <stevensreality@outlook.com>
2024-07-24 01:41:15 +0000

    commit: Test env setting

--- manage.py
+++ manage.py
@@ -12,8 +12,18 @@ def main():
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.development')
     elif config('ENVIRONMENT') == 'PRODUCTION':
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.production')
-    else:
+    elif config('ENVIRONMENT') == 'WORKSPACE':
         os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.workspace')
+    elif os.environ.get('ENVIRONMENT') == 'WORKSPACE':
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aob.settings.workspace')
+    else:
+        try:
+            from django.core.exceptions import ImproperlyConfigured
+        except ImproperlyConfigured:
+            raise ImproperlyConfigured(
+            "Couldn't detect current environment."
+            "Please set the ENVIRONMENT variable to either 'DEVELOPMENT', 'PRODUCTION', or 'WORKSPACE'"
+        )
         
         
         
