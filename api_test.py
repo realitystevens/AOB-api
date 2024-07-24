9c9ae548f38122592ff42074ad79ee258ca1ee0a
Reality Stevens <stevensreality@outlook.com>
2024-07-24 00:10:21 +0000

    Commit 'api_test.py'

---
+++ api_test.py
@@ -0,0 +1,17 @@
+import requests
+from decouple import config
+
+
+url = "https://127.0.0.1:8000/api/products/"
+
+API_KEY = config('API_KEY')
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization" : "Token f{API_KEY}"
+}
+
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
